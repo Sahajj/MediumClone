eef40fc604bdc95c987ac8cfd6525d7285af82ae
sahaj9 <sahajjain739739@gmail.com>
2024-03-16 17:23:01 +0530

    Updates

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React  from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Bounce } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -20,6 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       draggable
       pauseOnHover
       theme="dark"
-      transition: Bounce/>
+      transition= {Bounce}/>
   </React.StrictMode>,
 )
